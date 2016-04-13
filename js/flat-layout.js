2ea5dc6a5209b6e60108edfc869d8778e0733341
aadams22 <ashleighadams@Ashleighs-Air.netgear.com>
2016-04-12 23:41:02 -0400

    cleaning up css

--- js/flat-layout.js
+++ js/flat-layout.js
@@ -3,9 +3,6 @@ $(function(){
 	//appends the new div overlays
 	function createNewFigures() {
 		$('.cube-container').append(
-			"<figure class='flat-figure right' id='enigmatic'>" + 
-		 		"<div class='disabled'>Enigmatic: an AES 256 encrypted private messaging web application using Node.js, Express, Socket.io, MongoDB, Crypto, Passport Authentication, and Facebook Oauth</div>" +
-		 	"</figure>" +
 
      "<figure class='flat-figure right' id='medtrakr'>" +
       	"<div class='disabled'>MedTrakr: a personal medication tracker using Node.js, Express, jQuery, Passport Authentication</div>" +
@@ -15,28 +12,36 @@ $(function(){
       	"<div class='disabled'>Connect Four: a static website using HTML5, CSS3, Javascript, jQuery</div>" +
       "</figure>" +
 
+      "<figure class='flat-figure right' id='oli-and-olive'>" +
+      	"<div class='disabled'>Oli & Olive: a personalized landing page using Ruby, Rails, Devise, and the DarkSkies API</div>" +
+      "</figure>" +
+
       "<figure class='flat-figure right' id='space'>" +
       	"<div class='disabled'>Space: a single page educational 3D animated model of the Solar System using HTML5, CSS3, Angular, Node.js, Express, MongoDB, Passport Authentication</div>" +
       "</figure>" +
 
-      "<figure class='flat-figure right' id='oli-and-olive'>" +
-      	"<div class='disabled'>Oli & Olive: a personalized landing page using Ruby, Rails, Devise, and the DarkSkies API</div>" +
-      "</figure>");
-		$("<ul>" + "<li class='left-right' id='l'>L</li>" + "<li id='r'>R</li>" + "</ul>").insertAfter('.cube-container');
+      "<figure class='flat-figure left' id='enigmatic'>" + 
+		 		"<div class='disabled'>Enigmatic: an AES 256 encrypted private messaging web application using Node.js, Express, Socket.io, MongoDB, Crypto, Passport Authentication, and Facebook Oauth</div>" +
+		 	"</figure>");
+
+		$("<ul class='left-right'>" + "<li id='l'>&#xE805</li>" + "<li id='r'>&#xE806</li>" + "</ul>").insertAfter('.cube-container');
 	
 	$('#l').on('click', function(e){
-		console.log('left clicked');
+
 		var $right = $('.right').last();
-		console.log('RIGHT CLASS: ', $right);
 		$right.removeClass('right');
 		$right.addClass('left'); 
+
+
 	});
 
 	$('#r').on('click', function(e){
-		console.log('right clicked');
 		var $left = $('.left').last();
 		$left.removeClass('left');
 		$left.addClass('right'); 
+
+
+
 	});
 
 	};
